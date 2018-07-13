cf636b5ba3420431a6c1135f69ecba21be5b81c0
yanghao <yanghao@do1.com.cn>
2018-07-13 16:12:39 +0800

    路由拦截中间件

---
+++ middleware/route-filter/index.js
@@ -0,0 +1,39 @@
+/**
+ * Created by admin on 2018/7/13.
+ *
+ * 路由拦截的中间件，可以拦截所有请求
+ *
+ */
+
+module.exports = routerFilter;
+
+/**
+ * 拦截路由
+ * @param options
+ * @returns {routerFilter}
+ */
+function routerFilter(app){
+    return function routerFilter(req,res,next){
+        if(!app){
+            return next();
+        }
+
+        //排除在路由之外的路径
+        var exclude = app.get('exclude');
+        var loginPath = app.get('login');
+        var url = req.originalUrl;
+
+        if(exclude.indexOf(url)>=0){
+            // 公共部分放行通过
+            return next();
+
+        }else if(req.session.user){
+            //包含session的放行
+            return next();
+
+        }else if(!req.session.user){
+            //没有session，返回到登录页面
+            res.redirect(loginPath);
+        }
+    }
+}
