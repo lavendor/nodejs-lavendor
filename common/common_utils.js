6052dfe5d6a3cf0ac5537edc5141770034bb64f2
yanghao <yanghao@do1.com.cn>
2018-07-19 11:02:56 +0800

    数组转树型JSON

--- common/common_utils.js
+++ common/common_utils.js
@@ -2,48 +2,35 @@
  * Created by Administrator on 2017/8/25.
  */
 
-var phantom = require('phantom');
-var pdf = require('html-pdf');
-var wkhtmltopdf = require('wkhtmltopdf');
-var fs = require('fs');
 
 /**
- * 使用wkhtmltopdf转换
- * @param html
- * @param pdfName
+ * 数组转树型数据结构
+ * @param a
+ * @returns {Array}
  */
-exports.wkHtmlToPdf = function(html,pdfName){
-    wkhtmltopdf.command = 'D:/Program Files/wkhtmltopdf/bin/wkhtmltopdf.exe';
-    wkhtmltopdf(html,{output:'dash.pdf'});
+exports.getArrToTree  = function(a){
+    var result = [], hash = {};
+    for(var i=0; i < a.length; i++){
+        //使用ID作为键，ID对应的对象作为值，存在哈希对象中
+        hash[a[i]['id']] = a[i];
+    }
+    for(var j=0; j < a.length; j++){
+        //当前对象
+        var currentValue = a[j];
+
+        //使用当前对象的父ID，从哈希对象中拿到当前对象的父对象
+        var parentValue = hash[currentValue['pid']];
+
+        //有父对象
+        if(parentValue){
+            //判断父对象是不是包含了子节点，没包含，则创建子节点
+            !parentValue['children'] && (parentValue['children'] = []);
+            //把当前对象归为父对象的子节点中
+            parentValue['children'].push(currentValue);
+        }else{
+            //没有父对象，则为根节点
+            result.push(currentValue);
+        }
+    }
+    return result;
 }
-
-/**
- * html to pdf 排版略有问题
- * @param url
- * @param pdfName
- */
-exports.renderHtml2Pdf = function (url,pdfName){
-    phantom.create().then(function(ph) {
-        ph.createPage().then(function(page) {
-            page.open("http://127.0.0.1:8080/"+url+"").then(function(status) {
-                page.render(__dirname+'/'+pdfName).then(function() {
-                    console.log('Page Rendered');
-                    ph.exit();
-                });
-            });
-        });
-    });
-};
-
-/**
- * 这种方法没有渲染样式和图片
- * @param url
- * @param pdfName
- */
-exports.html2Pdf = function(html,pdfName){
-    var options = {format:true};
-    pdf.create(html,options).toFile(__dirname+'/'+pdfName,function(err,res){
-        if (err) return console.log(err);
-        console.log(res);
-    });
-};
