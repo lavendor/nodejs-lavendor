684a71cc78b2ad4fcea408c04baa709aeef1e2b7
yanghao <yanghao@do1.com.cn>
2018-07-24 17:43:07 +0800

    index不传值

--- routes/index/index.js
+++ routes/index/index.js
@@ -2,8 +2,6 @@
  * Created by Administrator on 2018/7/14.
  */
 var express = require('express'),
-    menuService = require('../../model/menu/menu_services'),
-    commonUtlis = require('../../common/common_utils'),
     router = express.Router();
 
 /**
@@ -11,17 +9,7 @@ var express = require('express'),
  * 初始化菜单
  */
 router.get('/', function (req, res) {
-    menuService.getMenuList().then(function(lists){
-        var menuTrees = commonUtlis.getArrToTree(lists,'_id','menu_parent');
-        res.render('app/index/index',{
-            menuTrees:menuTrees
-        });
-    }).catch(function(err){
-        res.render('app/index/index',{
-            menuTrees:null,
-            message:err.message
-        });
-    });
+    res.render('app/index/index');
 });
 
 
