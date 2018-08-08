bcc6b7d524e22b900fb8f6dc4bbb5514a9926d5c
yanghao <yanghao@do1.com.cn>
2018-08-08 19:49:01 +0800

    权限分配页面，使用jstree插件

--- lavendor/routes/permit/permit.js
+++ lavendor/routes/permit/permit.js
@@ -3,13 +3,22 @@
  */
 
 var express = require('express'),
-    router= express.Router();
+    router= express.Router(),
+    commonUtils = require('../../../common/commonUtils'),
+    menuServices = require('../../services/menu/menu_services');
 
 /**
  * 跳转到权限页面
  */
 router.get('/',function(req,res){
     res.render('app/permit/permit_list');
-})
+});
+
+/**
+ * 获取菜单树
+ */
+router.get('/getMenuTree',function(req,res){
+    commonUtils.respJSON(res,menuServices.menuTree());
+});
 
 module.exports = router;
